90ad0e198a77a9cd9bd4ce027eb249365f384f5a
juu124 <juu12424@gmail.com>
2026-02-05 16:14:33 +0900

    1차 실습 : user 정보 입력 앱 (toolbar, menu 사용)

--- app/src/main/java/com/example/mission1/MainActivity.kt
+++ app/src/main/java/com/example/mission1/MainActivity.kt
@@ -1,20 +1,56 @@
 package com.example.mission1
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mission1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var addLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Initialize the launcher
+        addLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    // This is where you handle the data returned from AddStudentActivity
+                    val data: Intent? = result.data
+                    // e.g., val studentName = data?.getStringExtra("name")
+                }
+            }
+    }
+
+    override fun onCreateOptionsMenu(menu: android.view.Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menuMainAdd) {
+            val intent = Intent(this, AddStudentActivity::class.java)
+            addLauncher.launch(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
